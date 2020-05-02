90313b90f650cf22c7df1e0d8f59a377b72fdfd8
GiulianoDias <giuliano.phocus@gmail.com>
2020-05-02 09:43:07 -0300

    Implementando classes 'Contas e Lancamentos'

---
+++ AppFinanceiro/FormContas.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFinanceiro {
+    class FormContas {
+
+        public enum Grupos { receita, despesa}
+
+        protected Grupos _Grupo;
+        protected int _Id;
+        protected string _Conta;
+
+        #region Codigos de Acesso
+
+        public int Id {
+            set { _Id = value; }
+            get { return _Id; }
+        }
+
+        public Grupos Grupo  {
+            set { _Grupo = value;}
+            get { return _Grupo; }
+        }
+
+        public string Conta {
+            set { _Conta = value; }
+            get { return _Conta; }
+        }
+
+        #endregion
+
+        #region Construtores
+
+        public FormContas() {
+            _Id = 0;
+            _Conta = "";
+            _Grupo = Grupos.despesa;
+        }
+
+        public FormContas(string conta, Grupos grupo) {
+            _Id = 0;
+            _Conta = conta;
+            _Grupo = grupo;
+        }
+
+        public FormContas(int id, string conta, Grupos grupo) {
+            _Id = id;
+            _Conta = conta;
+            _Grupo = grupo;
+        }
+        #endregion
+    }
+}
---
+++ AppFinanceiro/FormLancamentos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Runtime;
+
+
+
+namespace AppFinanceiro {
+    class FormLancamentos : FormContas {
+
+        static List<FormLancamentos> FormLancamento = new List<FormLancamentos>();
+        static int MaxId = 0;
+
+        protected int _IdL;
+        protected int _Dia;
+        protected int _Mes;
+        protected int _Ano;
+        protected int _Valor;
+        protected string _Descricao;
+
+        #region Acesso a Dados
+
+        public int IdL {
+            set { _IdL = value; }
+            get { return _IdL; }
+        }
+
+
+        #endregion
+
+        #region Construtores
+        #endregion
+
+        #region Metodos
+        #endregion
+
+
+
+    }
+}
